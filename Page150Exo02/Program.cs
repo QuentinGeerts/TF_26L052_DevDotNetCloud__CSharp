09ef1e53743f07b5182917799c0f5e32109622c2
Quentin Geerts <quentin.geerts@bstorm.be>
2026-08-17 14:22:14 +0200

    Correction - Factorielle d'un nombre

---
+++ Page150Exo02/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Calculer le factoriel d’un nombre entré au clavier.
+ */
+using System.Numerics;
+
+bool isValid = false;
+int number = 0;
+
+Console.WriteLine($"Entrez un nombre positif :");
+while (!isValid)
+{
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("Ce n'est pas un nombre, réessayez :");
+    }
+    isValid = number >= 0;
+    if (!isValid) Console.WriteLine($"Vous devez entrer un nombre strictement positif, réessayez :");
+}
+
+if (number == 0)
+    Console.WriteLine($"{number}! = 1");
+else
+{
+    BigInteger factorielle = 1;
+
+    for (int i = 2; i <= number; i++)
+    {
+        factorielle *= i;
+    }
+
+    Console.WriteLine($"{number}! = {factorielle}");
+}
